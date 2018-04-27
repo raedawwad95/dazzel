25e397b8de33980f90f9f9932f9f1130aff1273a
nada <snada.g14@gmail.com>
2018-04-27 09:11:11 -0700

    Indentation-server

--- server/index.js
+++ server/index.js
@@ -23,7 +23,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // use session middleware and configure it
 app.use(session({secret:'mysecretsession',resave:true,saveUninitialized: true,
-
    store:new mongoStore({mongooseConnection: mongoose.connection,collection: 'session',})
  }))
 
@@ -53,71 +52,75 @@ app.get('/admin/doctorform',function(req,res){
 
 app.post('/admin/doctorform',function(req,res){
   //convert adress from string to object that have two key lat and lng
-  var latlngStr=req.body.address
-  var latlngObj={
-    lat:parseFloat(latlngStr.split(",")[0]),
-    lng:parseFloat(latlngStr.split(",")[1])
-  };
-// we have three option here 
-  if(req.body.action==="Add doctor"){
+	var latlngStr=req.body.address
+	var latlngObj={
+	lat:parseFloat(latlngStr.split(",")[0]),
+	lng:parseFloat(latlngStr.split(",")[1])
+	};
+	// we have three option here 
+	if(req.body.action==="Add doctor"){
   
   //create a variable doctor_data hold all new data
-  var doctor_data={
-    name:req.body.name,
-    specialization:req.body.specialization,
-    address:latlngObj,
-    tel:req.body.tel,
-    rate:req.body.rate
-  }
+		 var doctor_data={
+		    name:req.body.name,
+		    specialization:req.body.specialization,
+		    address:latlngObj,
+		    tel:req.body.tel,
+		    rate:req.body.rate
+	  	}
 
   //insert doctor_data to the database
- var newDoc=new dataModels.Doctor(doctor_data);
+ 		var newDoc=new dataModels.Doctor(doctor_data);
 
- newDoc.save(function(err,doc){
-  if(err){
-  	console.log("error in saving a new doctor");
-    res.status(500);
-  	res.send("error in saving DB")
-  }
-  else{
-    res.status(302);
-    res.redirect('/admin/doctorform');
-  }
-
- })
-}else if(req.body.action==="Delete doctor"){
+		 newDoc.save(function(err,doc){
+	 	 if(err){
+		  	console.log("error in saving a new doctor");
+		    res.status(500);
+		  	res.send("error in saving DB")
+		  }
+	  	else{
+		    res.status(302);
+		    res.redirect('/admin/doctorform');
+	  	}
+	 	})
+	}
+	else if(req.body.action==="Delete doctor"){
   // delete doctor by finding his name and delete it{ name } using deleteOne
-  dataModels.Doctor.deleteOne({ 'name': req.body.name },  function (err, doctor) {
-  if (err) {
-    return handleError(err)
-  }else{
-    res.status(302);
-    res.render('doctorform');
-  };
-});
-}else{
+		dataModels.Doctor.deleteOne({ 'name': req.body.name },  function (err, doctor) {
+	  		if (err) {
+	    		return handleError(err)
+	  		}
+	  		else {
+			    res.status(302);
+			    res.render('doctorform');
+		 	 };
+		});
+	}
+	else {
   // modify doctor by finding his name and modify it{ name } using findone and modify data in result  
-  dataModels.Doctor.findOne( { "name":req.body.name}, function(err, result){
-       if (!err && result) {
-        result.specialization = req.body.specialization; // update ur values goes here
-        result.address = latlngObj;
-        result.tel = req.body.tel;
-        result.rate = req.body.rate;
-        var newDoctor = new dataModels.Doctor(result);
-        newDoctor.save(function(err, result2){
-            if(!err) {
-               res.render('doctorform')
-            } else {
-            	 res.status(500);
-	             res.send(err);
-              }
-        })  
-       } else {
-       	res.status(500);
-       	res.send(err);
-       }
-    }); 
-}
+			dataModels.Doctor.findOne( { "name":req.body.name}, function(err, result){		
+	     		if (!err && result) {
+			        result.specialization = req.body.specialization; // update ur values goes here
+			        result.address = latlngObj;
+			        result.tel = req.body.tel;
+			        result.rate = req.body.rate;
+			        var newDoctor = new dataModels.Doctor(result);
+		     	   	newDoctor.save(function(err, result2){
+		           		if(!err) {
+		            		 res.render('doctorform')
+		            	} 
+		            	else {
+		            		res.status(500);
+			            	res.send(err);
+		           		 }
+		   			})  
+		       } 
+	       		else {
+			       	res.status(500);
+			       	res.send(err);
+	       		}
+	   	 	}); 
+		}
 })
 
 app.post('/admin/signup',passport.authenticate('local.signup',{
@@ -140,36 +143,37 @@ app.post('/admin/login',passport.authenticate('local.login',{
 app.get('/doctors/:rateSpic', function (req, res) {
     dataModels.Doctor.find({specialization:req.params.rateSpic},function(err, data) {
     if(err) {
-      res.sendStatus(500);
-    } else {
-      res.send(data);
+    	res.sendStatus(500);
+    } 
+    else {
+    	res.send(data);
     }
   }).limit(3).sort( { rate: -1} );
 });
 
 // get all nearest doctors based on specialty
 app.get('/docNearst/:spic', function (req, res) {
-  dataModels.Doctor.find({specialization:req.params.spic},function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.status(200);
-      res.send(data);
+	dataModels.Doctor.find({specialization:req.params.spic},function(err, data) {
+	if(err) {
+     	res.sendStatus(500);
+    } 
+    else {
+	    res.status(200);
+	    res.send(data);
     }
   })
 });
 
 
-
 app.get('/doctors',function(req,res){
-  dataModels.Doctor.find({},function(err,data){
-    if(err){
-      res.sendStatus(500);
-    }
-    else{
-      res.sendStatus(200)
-    }
-})
+	dataModels.Doctor.find({},function(err,data){
+   		if(err){
+    		res.sendStatus(500);
+    	}
+    	else{
+    		res.sendStatus(200)
+   		 }
+	})
 })
 
 app.listen(process.env.PORT||3000, function() {
