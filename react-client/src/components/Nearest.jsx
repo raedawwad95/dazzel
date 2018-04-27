767023011e984cefca84538dfcd2fe18ab5d1afa
nada <snada.g14@gmail.com>
2018-04-27 02:33:51 -0700

    Change the distance metric

--- react-client/src/components/Nearest.jsx
+++ react-client/src/components/Nearest.jsx
@@ -16,39 +16,55 @@ class Map extends React.Component {
       }
 
     this.showNearest=this.showNearest.bind(this);
+    this.getDistance=this.getDistance.bind(this);
+    this.deg2rad=this.deg2rad.bind(this);
   }
+
+// Haversine distance-"determines the great-circle distance between two points on a sphere given their longitudes and latitudes"
+  getDistance(lat1,lng1,lat2,lng2) {
+  var earthRadius = 6371; // Radius of the earth in km
+  var dLat = this.deg2rad(lat2-lat1);  
+  var dLng = this.deg2rad(lng2-lng1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
+    Math.sin(dLng/2) * Math.sin(dLng/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = earthRadius* c; // Distance in km
+  return d;
+}
+
+deg2rad(deg) {
+  return deg * (Math.PI/180);
+}
 
 /*
 * Ajax request fetch the nearest three Doctors from  the data base  
 */
  showNearest(){
-   console.log(this.props.lat,this.props.lng)
+   //console.log(this.props.lat,this.props.lng)
     
       var that=this
       $.ajax({
         url:'/docNearst/'+that.props.specialty,
         type:'GET',
         success:function(data){
-          console.log(data)
           var arr=[]
           for (var i = 0; i < data.length; i++) {
-            var distance=0;
-            distance=Math.sqrt(Math.pow((that.props.lat-data[i].address.lat), 2)+Math.pow((that.props.lng-data[i].address.lng),2))
-            console.log(distance);
-            data[i].des = distance
-            arr.push(data[i])  
+            var distance=0
+            distance=that.getDistance(data[i].address.lat,that.props.lat,data[i].address.lng,that.props.lng);
+            data[i].des = distance;
+            arr.push(data[i]);
           }
           arr.sort(function(a, b){return a.des - b.des});
           arr.splice(3,arr.length-1)
-
-
           that.setState({
             specialties:arr
           }) 
-        }
+         }
       })
-    
-  }
+      }
 /*
 * function gives the lat and lng when the user click on his location 
 */
